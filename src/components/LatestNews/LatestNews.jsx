cf3151737062266ff9ebcc961135ee19f19f1441
subaru300 <s.n.cherep@gmail.com>
2024-01-24 13:30:36 +0200

    add design for web

---
+++ src/components/LatestNews/LatestNews.jsx
@@ -0,0 +1,13 @@
+import BannersList from '../BannersList/BannersList';
+import styles from './LatestNews.module.css';
+
+
+const LatestNews = ({ banners, isLoading }) => {
+    return (
+<section className={styles.section}>
+        <BannersList banners={banners} isLoading={isLoading}/>
+</section>
+    )
+};
+
+export default LatestNews;
