9611ad504594603c40b5786955b5f9598e2299a3
subaru300 <s.n.cherep@gmail.com>
2024-05-26 15:19:31 +0300

    fix imports

--- src/entities/news/ui/NewsItem/NewsItem.tsx
+++ src/entities/news/ui/NewsItem/NewsItem.tsx
@@ -1,6 +1,6 @@
 import { formatTimeAgo } from '@/shared/helpers/formatTimeAgo';
-import { INews } from '../../interfaces';
 import styles from './NewsItem.module.css';
+import { INews } from '../../model/types';
 
 interface Props {
   item: INews;
